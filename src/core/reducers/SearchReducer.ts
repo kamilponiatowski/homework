e3747f90374148c2a544faab8d064ad68067193d
Mateusz Kulesza <mateusz@altkomakademia.pl>
2021-05-15 10:06:39 +0200

    useReducer + TS

---
+++ src/core/reducers/SearchReducer.ts
@@ -0,0 +1,41 @@
+import { Reducer } from "react";
+import { AlbumView } from "../../model/Search";
+
+interface State { query: string, isLoading: boolean, message: string, results: AlbumView[] }
+type Actions =
+    | SEARCH_START
+    | SEARCH_SUCCESS
+    | SEARCH_FAILED;
+
+type SEARCH_START = { type: 'SEARCH_START', payload: { query: string } }
+type SEARCH_SUCCESS = { type: 'SEARCH_SUCCESS', payload: { results: AlbumView[] } }
+type SEARCH_FAILED = { type: 'SEARCH_FAILED', payload: { error: Error } }
+
+export const searchStart = (query: string): SEARCH_START => ({ type: 'SEARCH_START', payload: { query } })
+export const searchSuccess = (results: AlbumView[]): SEARCH_SUCCESS => ({ type: 'SEARCH_SUCCESS', payload: { results } })
+export const searchFailed = (error: Error): SEARCH_FAILED => ({ type: 'SEARCH_FAILED', payload: { error } })
+
+export const initialState = { query: '', isLoading: false, message: '', results: [] as AlbumView[] }
+// Extract Types from existing JS objects and functions:
+// type State = typeof initialState;
+// type SEARCH_START = ReturnType<typeof searchStart>;
+
+const reducer: Reducer<State, Actions> = (state, action) => {
+
+    switch (action.type) {
+        case 'SEARCH_START': return {
+            ...state, isLoading: true, query: action.payload.query, message: ''
+        }
+        case 'SEARCH_SUCCESS': return {
+            ...state, isLoading: false, results: action.payload.results
+        }
+        case 'SEARCH_FAILED': return {
+            ...state, isLoading: false, message: action.payload.error?.message || 'Unexpected Error'
+        }
+        default:
+            return state
+    }
+
+}
+
+export default reducer
--- src/music-search/containers/MusicSearchView.tsx
+++ src/music-search/containers/MusicSearchView.tsx
@@ -1,25 +1,32 @@
-import React, { useCallback, useEffect, useRef } from 'react'
-import { Album, AlbumView } from '../../model/Search'
-import { AlbumGrid } from '../components/AlbumGrid'
-import { SearchForm } from '../../core/components/SearchForm'
-import { fetchAlbums, fetchArtists, useSearchAlbums } from '../../core/hooks/useSearchAlbums'
-import { useFetch } from '../../core/hooks/useFetch'
-import { RouteComponentProps, useHistory, useLocation } from 'react-router'
+import React, { Reducer, useCallback, useEffect, useReducer, useRef } from 'react'
+import { useHistory, useLocation } from 'react-router'
+import { SearchForm } from '../../core/components/SearchForm';
+import { fetchAlbums } from '../../core/hooks/useSearchAlbums';
+import reducer, { initialState, searchFailed, searchStart, searchSuccess } from '../../core/reducers/SearchReducer';
+import { AlbumView } from '../../model/Search';
+import { AlbumGrid } from '../components/AlbumGrid';
 
 interface Props { }
 
 export const MusicSearchView = (props: Props) => {
-    const [{ isLoading, message, results, params: query }, setQuery] = useFetch(fetchAlbums)
-    const ref = useRef<{ reset(): void } | null>(null)
+    // const [{ isLoading, message, results, params: query }, setQuery] = useFetch(fetchAlbums)
+    // const [placki, setPlacki] = useState('placki')
+    const [{ isLoading, message, query, results }, dispatch] = useReducer(reducer, initialState)
 
+    const ref = useRef<{ reset(): void } | null>(null)
     const { push, replace } = useHistory()
     const { search: searchParams } = useLocation()
 
-
     useEffect(() => {
         const q = new URLSearchParams(searchParams.slice(1)).get('q')
+        if (!q) { return; }
         window.document.title = 'Searching ' + q
-        setQuery(q)
+
+        dispatch(searchStart(q))
+        fetchAlbums(q)
+            .then(res => dispatch(searchSuccess(res)))
+            .catch(error => dispatch(searchFailed(error)))
+
     }, [searchParams])
 
     const search = useCallback((query) => {
