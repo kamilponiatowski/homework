e32a8d0cd654aff2ecc55e497ef7cac0ccd005ab
Kamil Poniatowski <poniatowski.dev@gmail.com>
2021-05-15 21:12:09 +0200

    adding styles for displaying tracks for a selected album

--- src/music-search/containers/AlbumDetails.tsx
+++ src/music-search/containers/AlbumDetails.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useParams } from 'react-router'
 import { fetchAlbumById } from '../../core/hooks/usePlaylists'
@@ -8,6 +8,30 @@ import { AlbumCard } from '../components/AlbumCard'
 
 interface Props {
 
+}
+
+// function msToTime(duration: number) {
+//     var milliseconds = Math.floor((duration % 1000) / 100),
+//         seconds = Math.floor((duration / 1000) % 60),
+//         minutes = Math.floor((duration / (1000 * 60)) % 60),
+//         hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
+
+//     hours = (hours < 10) ? "0" + hours : hours;
+//     minutes = (minutes < 10) ? "0" + minutes : minutes;
+//     seconds = (seconds < 10) ? "0" + seconds : seconds;
+
+//     return hours + ":" + minutes + ":" + seconds + "." + milliseconds;
+// }
+
+function msToTime(ms: number) {
+    let seconds = (ms / 1000).toFixed(1);
+    let minutes = (ms / (1000 * 60)).toFixed(1);
+    let hours = (ms / (1000 * 60 * 60)).toFixed(1);
+    let days = (ms / (1000 * 60 * 60 * 24)).toFixed(1);
+    if (+seconds < 60) return seconds + " Sec";
+    else if (+minutes < 60) return minutes + " Min";
+    else if (+hours < 24) return hours + " Hrs";
+    else return days + " Days"
 }
 
 export const AlbumDetails = (props: Props) => {
@@ -31,13 +55,14 @@ export const AlbumDetails = (props: Props) => {
     const { isLoading, message } = useSelector(selectAlbumFetchState)
     const album = useSelector(selectAlbum)
     const { album_id } = useParams<{ album_id: string }>()
+    const [album2, setAlbum2] = useState<any>()
 
     useEffect(() => {
 
         dispatch(fetchAlbumStart(album_id))
 
         fetchAlbumById(album_id)
-            .then(data => { dispatch(fetchAlbumSuccess(data)) })
+            .then(data => { dispatch(fetchAlbumSuccess(data)); setAlbum2(data) })
             .catch(error => { dispatch(fetchAlbumFailed(error)) })
     }, [album_id])
 
@@ -50,24 +75,17 @@ export const AlbumDetails = (props: Props) => {
     return (
         <div>
             <div className="row">
-                <div className="col">
-                    <small className="text-muted">{album_id}</small>
-                    <h1>{album?.name}</h1>
-                </div>
-            </div>
-            <div className="row">
-                <div className="col">
-                    {album && <AlbumCard album={album} />}
-                </div>
-                <div className="col">
-
-                    <dl>
+                <div className="col-5">
+                    <dl className="col d-flex flex-column">
                         <dt>Album name:</dt>
                         <dd>{album?.name}</dd>
 
                         <dt>Artist:</dt>
                         <dd>{album?.artists[0]?.name}</dd>
                     </dl>
+                    {album && <AlbumCard album={album} />}
+                </div>
+                <div className="col-7">
 
                     {/* 
                         TODO:
@@ -80,12 +98,39 @@ export const AlbumDetails = (props: Props) => {
 
                     <SelectPlaylist playlists={[]} onSelect={() => { }} />
 
-                    <h3>Tracks</h3>
-                    {album?.tracks && album.tracks.items.map(track =>
-                        <p>
-                            .... (+) add to selected playlist
-                        </p>
-                    )}
+                    <h3 className="mt-2">Tracks {album?.tracks?.total ? `(${album.tracks.total})` : null}</h3>
+                    {album?.tracks &&
+                        <table className="table table-hover">
+                            <thead>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">Title</th>
+                                    <th scope="col">
+                                        <div className="d-flex justify-content-center">
+                                            <span className="material-icons">
+                                                schedule
+                                            </span>
+                                        </div>
+                                    </th>
+                                    <th scope="col">Add</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {album.tracks.items.map((track, index) => <tr>
+                                    <th scope="row">{++index}</th>
+                                    <td>
+                                        <a href={track.external_urls.spotify}>{track.name}</a>
+                                    </td>
+                                    <td className="d-flex justify-content-center text-nowrap">{msToTime(track.duration_ms)}</td>
+                                    <td>
+                                        <span className="material-icons">
+                                            add
+                                        </span>
+                                    </td>
+                                </tr>)}
+                            </tbody>
+                        </table>
+                    }
                     {!album?.tracks &&
                         <div className="alert alert-info" role="alert">
                             No tracks to display
